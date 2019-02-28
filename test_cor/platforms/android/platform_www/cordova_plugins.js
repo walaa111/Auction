c9189578d6733aee8780fada1d398f1cf83f6b6c
walaa111 <walaagamil18@gmail.com>
2019-02-28 19:08:05 +0200

    add

--- test_cor/platforms/android/platform_www/cordova_plugins.js
+++ test_cor/platforms/android/platform_www/cordova_plugins.js
@@ -8,23 +8,13 @@ module.exports = [
       "cordova.InAppBrowser.open",
       "window.open"
     ]
-  },
-  {
-    "id": "cordova-plugin-admob-simple.AdMob",
-    "file": "plugins/cordova-plugin-admob-simple/www/AdMob.js",
-    "pluginId": "cordova-plugin-admob-simple",
-    "clobbers": [
-      "window.plugins.AdMob"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-inappbrowser": "3.0.0",
-  "cordova-admob-sdklibs": "2.1.6",
-  "cordova-plugin-admob-simple": "3.3.8"
+  "cordova-plugin-inappbrowser": "3.0.0"
 };
 // BOTTOM OF METADATA
 });
